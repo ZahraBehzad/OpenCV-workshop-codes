45b79bfe909d3b1d3fd31dd3dd3960d8169ffba6
ZahraBehzad <zhrabhzad0@gmail.com>
2025-03-29 18:13:04 +0330

    Update Numpy-Introduction.py

--- OpenCV workshop codes/Numpy-Introduction/Numpy-Introduction.py
+++ OpenCV workshop codes/Numpy-Introduction/Numpy-Introduction.py
@@ -92,3 +92,11 @@ print(a[2:5])
 print(a[:6:2])
 print(a[::-1])
 
+def f(x, y):
+    return 10 * x + y
+b = fromfunction(f, (5, 4), dtype=int)
+print(b)
+print(b[2, 3])
+print(b[0:5, 1])
+print(b[:, 1])
+print(b[1:3, :])
