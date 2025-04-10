6628c5189bd557f98029489a205e8d3e7a06e7a7
ZahraBehzad <zhrabhzad0@gmail.com>
2025-04-11 00:40:06 +0330

    save video

--- OpenCV workshop codes/OpenCV-Introduction/OpenCV-video.py
+++ OpenCV workshop codes/OpenCV-Introduction/OpenCV-video.py
@@ -1,7 +1,6 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
-
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -13,7 +12,28 @@ while(True):
     cv2.imshow('frame', gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 # When everything done, release the capture
 cap.release()
+cv2.destroyAllWindows()
+
+
+
+#___________________________ Saving the video ___________________________
+cap = cv2.VideoCapture(0)
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('OpenCV workshop codes/OpenCV-Introduction/output.avi', fourcc, 20.0, (640, 480))
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == True:
+        frame = cv2.flip(frame, 0)
+        # Write the flipped frame
+        out.write(frame)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+cap.release()
+out.release()
 cv2.destroyAllWindows()
