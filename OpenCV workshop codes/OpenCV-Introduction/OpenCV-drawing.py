9c5d48445631b6f1bb44e210dd07db297715fb03
ZahraBehzad <zhrabhzad0@gmail.com>
2025-04-11 00:56:34 +0330

    Update OpenCV-drawing.py

--- OpenCV workshop codes/OpenCV-Introduction/OpenCV-drawing.py
+++ OpenCV workshop codes/OpenCV-Introduction/OpenCV-drawing.py
@@ -16,6 +16,10 @@ img = cv2.ellipse(img, (256, 256), (100, 50), 0, 0, 180, 255, -1)
 pts = np.array([[10, 5], [20, 30], [70, 20], [50, 10]], np.int32)
 img = cv2.polylines(img, [pts], True, (0, 255, 255))
 
+# Text
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img, 'OpenCV', (10, 500), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyWindow('image')
