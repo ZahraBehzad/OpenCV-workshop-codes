a16e127a6cdfe65a5b6dd79727c399697a1cfe4b
ZahraBehzad <zhrabhzad0@gmail.com>
2025-04-10 23:46:44 +0330

    open cv codes added

---
+++ OpenCV workshop codes/OpenCV-Introduction/OpenCV-Introduction.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+
+# Load an image 
+img = cv2.imread("flower.jpg", cv2.IMREAD_COLOR)
+# Equivalent to: cv2.imread("image.jpg", 1)
+print(img.shape) # Output: (height, width, channels)
+
+# Load an color image in grayscale
+image_gray = cv2.imread("flower.jpg", cv2.IMREAD_GRAYSCALE)
+# Equivalent to: cv2.imread("image.jpg", 0)
+print(image_gray.shape)  # Output: (height, width)
+
+# Load an image with all channels
+image_unchanged = cv2.imread("/Test.png", cv2.IMREAD_UNCHANGED)
+# Equivalent to: cv2.imread("image.png", -1)
+print(image_unchanged.shape)  # Output: (height, width, 4) for PNG with alpha
