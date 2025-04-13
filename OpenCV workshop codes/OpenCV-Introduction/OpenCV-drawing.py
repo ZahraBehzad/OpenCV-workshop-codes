f76770531314f2f9d0373a8ca6eb3de16e1316b2
ZahraBehzad <zhrabhzad0@gmail.com>
2025-04-13 23:21:13 +0330

    mouse paint

--- OpenCV workshop codes/OpenCV-Introduction/OpenCV-drawing.py
+++ OpenCV workshop codes/OpenCV-Introduction/OpenCV-drawing.py
@@ -23,3 +23,19 @@ cv2.putText(img, 'OpenCV', (10, 500), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyWindow('image')
+
+
+#################### mouse as a paint brush ####################
+# mouse callback function
+def draw_circle (event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        cv2.circle(img, (x, y), 100, (255, 0, 0), -1)
+# create a black image, a window and bind the function to window
+img = np.zeros((512, 512, 3), np.uint8)
+cv2.namedWindow('image')
+cv2.setMouseCallback('image', draw_circle)
+while(1):
+    cv2.imshow('image', img)
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+cv2.destroyAllWindows()
